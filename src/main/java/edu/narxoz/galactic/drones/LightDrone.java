87e9f18f58260d4d5c51519f7568ef913fec79a0
aiiwave <aitolkyn.mukhamet@narxoz.kz>
2026-02-07 08:46:47 +0500

    Final: Galactic Drone Delivery project complete

--- src/main/java/edu/narxoz/galactic/drones/LightDrone.java
+++ src/main/java/edu/narxoz/galactic/drones/LightDrone.java
@@ -2,5 +2,5 @@ package edu.narxoz.galactic.drones;
 public class LightDrone extends Drone {
     public LightDrone(String id, double maxPayloadKg) { super(id, maxPayloadKg); }
     @Override public double speedKmPerMin() {
-        return 10.0; }
+        return 10.0;}
 }
